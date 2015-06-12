650379066c987b8786a8063a81dc410a44438101
lucio@asuka.local <lucio@asuka.local>
2015-06-12 17:07:52 +0400

    Initial Commit

---
+++ sVpo/sVpo.h
@@ -0,0 +1,19 @@
+//
+//  sVpo.h
+//  sVpo
+//
+//  Created by Luciano Colosio on 6/12/15.
+//  Copyright (c) 2015 Luciano Colosio. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for sVpo.
+FOUNDATION_EXPORT double sVpoVersionNumber;
+
+//! Project version string for sVpo.
+FOUNDATION_EXPORT const unsigned char sVpoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <sVpo/PublicHeader.h>
+
+
